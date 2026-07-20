1ab52590888c936d4c1640f07e5d41809b86829e
Suraj Air <surajair@gmail.com>
2026-07-20 16:17:58 +0530

    chore: new migration applied. removed unwanted table

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260716_182540_initial from './20260716_182540_initial';
+import * as migration_20260720_104628_remove_app_metadata from './20260720_104628_remove_app_metadata';
 
 export const migrations = [
   {
     up: migration_20260716_182540_initial.up,
     down: migration_20260716_182540_initial.down,
-    name: '20260716_182540_initial'
+    name: '20260716_182540_initial',
+  },
+  {
+    up: migration_20260720_104628_remove_app_metadata.up,
+    down: migration_20260720_104628_remove_app_metadata.down,
+    name: '20260720_104628_remove_app_metadata'
   },
 ];
